9487bbe3ba8f4ab2c56ae1e42270dc59c0eebf8b
Saif-041 <sp19-bse-041@cuilahore.edu.pk>
2022-08-30 00:53:29 +0500

    utcController updated
    create testUTC Api.

--- app/controllers/utc_controller.rb
+++ app/controllers/utc_controller.rb
@@ -1,9 +1,27 @@
 class UtcController < ApplicationController
+    before_action :active_drop
+    before_action :non_active_drop
+    
     before_action :find_user, only: [:user]
     respond_to :json
 
     def time
         render json: { time: '' + DateTime.now.utc.to_s }, status: :ok
+    end
+
+    def test
+        render json: {
+                            :"Active Drop" => {
+                                "total_amount": 0,
+                                "total_drop_locations": @active_drop.length,
+                                "data": @active_drop
+                            },
+                            :"Non-Active Drop" => {
+                                "total_amount": 0,
+                                "total_drop_locations": @non_active_drop.length,
+                                "data": @non_active_drop
+                            }
+                        }, status: :ok
     end
 
     def user
@@ -14,12 +32,20 @@ class UtcController < ApplicationController
         end
         
     end
-    
+
 
     private 
       
     def find_user
         User.find_by(email: params[:user][:email])
+    end
+
+    def active_drop
+        @active_drop = Drop.where(is_active: true)
+    end
+    
+    def non_active_drop
+        @non_active_drop = Drop.where(is_active: false)
     end
 
 end
