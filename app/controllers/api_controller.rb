579c1597234229b1e9d4e7dd84c5e8e3572a6ae7
Saif-041 <sp19-bse-041@cuilahore.edu.pk>
2022-09-07 03:52:02 +0500

    checking for error resolve or not
    error detail:
    heroku error:
    "Can't verify CSRF token authenticity."

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,5 @@
 class ApiController < ActionController::API
-    # protect_from_forgery with: :null_session
+    protect_from_forgery with: :null_session
     # skip_before_action :verify_authenticity_token
 
     before_action :configure_permitted_parameters, if: :devise_controller?
