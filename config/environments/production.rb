a373e0008dd11888910fa810740d99af86ddfddb
Saif-041 <sp19-bse-041@cuilahore.edu.pk>
2022-09-07 05:01:55 +0500

    again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # added by saif
-  config.session_store :cookie_store, expire_after: 1.year
+  config.session_store :cookie_store, expire_after: 1.year, domain: 'https://asadakram.herokuapp.com/users'
 
 
   # Include generic and useful information about system operation, but avoid logging too much
